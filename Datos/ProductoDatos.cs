3220375526a7588171dcaa707596e5e81574642e
LucasCoronado <coronadolucasm@gmail.com>
2026-06-05 23:58:53 -0300

    Mover consultas SQL de Negocio a Datos para Productos

---
+++ Datos/ProductoDatos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPComercio.Datos;
+using TPComercio.Dominio;
+
+namespace Datos
+{
+    public class ProductoDatos
+    {
+        public List<Producto> ListarTodos()
+        {
+            List<Producto> lista = new List<Producto>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id, Codigo, Nombre, StockActual FROM Productos");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Producto aux = new Producto();
+
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.StockActual = (int)datos.Lector["StockActual"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
--- Negocio/ProductoNegocio.cs
+++ Negocio/ProductoNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,32 +12,8 @@ namespace TPComercio.Negocio
     {
         public List<Producto> listar()
         {
-            List<Producto> lista = new List<Producto>();
-            AccesoDatos datos = new AccesoDatos();
-
-            try
-            {
-                datos.setearConsulta("SELECT Id, Codigo, Nombre, StockActual FROM Productos");
-                datos.ejecutarLectura();
-
-                while (datos.Lector.Read())
-                {
-                    Producto aux = new Producto();
-
-                    aux.Id = (int)datos.Lector["Id"];
-                    aux.Codigo = (string)datos.Lector["Codigo"];
-                    aux.Nombre = (string)datos.Lector["Nombre"];
-                    aux.StockActual = (int)datos.Lector["StockActual"];
-
-                    lista.Add(aux);
-                }
-
-                return lista;
-            }
-            finally
-            {
-                datos.cerrarConexion();
-            }
+            ProductoDatos datos = new ProductoDatos();
+            return datos.ListarTodos();
         }
     }
 }
