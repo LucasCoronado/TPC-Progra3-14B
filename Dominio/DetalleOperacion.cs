5385719fa0e0e874f7870e401fd3236a710f9b28
LucasCoronado <coronadolucasm@gmail.com>
2026-06-06 00:38:42 -0300

    Clase detalle para compra y venta

---
+++ Dominio/DetalleOperacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPComercio.Dominio;
+
+namespace Dominio
+{
+    public abstract class DetalleOperacion
+    {
+        public int Id { get; set; }
+        public Producto ProductoAsociado { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Subtotal { get; set; } // Cantidad * PrecioUnitario
+    }
+}
