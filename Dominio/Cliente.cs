c000afb69ae19e3da76100835783a1a160d8fe0d
LucasCoronado <coronadolucasm@gmail.com>
2026-06-06 00:04:41 -0300

    Clase abstracta base para Clientes y Proveedores con atributos comunes

--- Dominio/Cliente.cs
+++ Dominio/Cliente.cs
@@ -1,17 +1,15 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace TPComercio.Dominio
 {
-    public class Cliente
+    public class Cliente : Persona
     {
-        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Dni { get; set; }
-        public string Telefono { get; set; }
-        public string Email { get; set; }
     }
 }
--- Dominio/Proveedor.cs
+++ Dominio/Proveedor.cs
@@ -1,16 +1,15 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace TPComercio.Dominio
 {
-    public class Proveedor
+    public class Proveedor : Persona
     {
-        public int Id { get; set; }
         public string RazonSocial { get; set; }
         public string Cuit { get; set; }
-        public string Telefono { get; set; }
-        public string Email { get; set; }
+
     }
 }
