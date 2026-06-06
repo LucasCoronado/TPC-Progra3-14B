9e0867dc6852e5cf5fba6daa975d4cc4435a9101
LucasCoronado <coronadolucasm@gmail.com>
2026-06-05 23:53:42 -0300

    Mover consultas SQL de Negocio a Datos para Categorias

---
+++ Datos/CategoriaDatos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPComercio.Datos;
+using TPComercio.Dominio;
+
+namespace Datos
+{
+    public class CategoriaDatos
+    {
+
+        public List<Categoria> ListarTodos()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion from Categorias");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
--- Negocio/CategoriaNegocio.cs
+++ Negocio/CategoriaNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,30 +14,8 @@ namespace Negocio
 
         public List<Categoria> Listar()
         {
-            List<Categoria> lista = new List<Categoria>();
-            AccesoDatos datos = new AccesoDatos();
-
-            try
-            {
-                datos.setearConsulta("Select Id, Descripcion from Categorias");
-                datos.ejecutarLectura();
-
-                while (datos.Lector.Read())
-                {
-                    Categoria aux = new Categoria();
-                    aux.Id = (int)datos.Lector["Id"];
-                    aux.Descripcion = (string)datos.Lector["Descripcion"];
-
-                    lista.Add(aux);
-                }
-
-                return lista;
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
+            CategoriaDatos datos = new CategoriaDatos();
+            return datos.ListarTodos();
         }
 
     }
