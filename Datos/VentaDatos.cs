f88a2c33894883b8d8f198505ae15036fb240ec3
LucasCoronado <coronadolucasm@gmail.com>
2026-06-28 23:55:47 -0300

    Se implementa listado y filtros de historial de ventas

--- Datos/VentaDatos.cs
+++ Datos/VentaDatos.cs
@@ -1,6 +1,7 @@
-﻿using Dominio;
+using Dominio;
 using System;
 using System.Collections.Generic;
+using TPComercio.Dominio;
 
 namespace TPComercio.Datos
 {
@@ -10,7 +11,7 @@ namespace TPComercio.Datos
         {
             int idVentaGenerado = 0;
 
-                AccesoDatos datosVenta = new AccesoDatos();
+            AccesoDatos datosVenta = new AccesoDatos();
             try
             {
                 datosVenta.setearConsulta("INSERT INTO Ventas (IdCliente, Fecha, Total) OUTPUT inserted.Id VALUES (@IdCliente, GETDATE(), @Total)");
@@ -60,6 +61,170 @@ namespace TPComercio.Datos
             }
 
             return idVentaGenerado;
+        }
+
+        public List<Venta> ListarHistorial(string cliente = "", DateTime? desde = null, DateTime? hasta = null)
+        {
+            List<Venta> lista = new List<Venta>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                string consulta = "SELECT V.Id, V.Fecha, V.NumeroFactura, V.Total, C.Nombre as Cliente, U.NombreUsuario as Vendedor FROM Ventas V JOIN Clientes C ON V.IdCliente = C.Id LEFT JOIN Usuarios U ON V.IdUsuario = U.Id WHERE 1=1 ";
+
+                if (!string.IsNullOrEmpty(cliente))
+                {
+                    consulta += " AND C.Nombre LIKE @Cliente ";
+                    datos.setearParametro("@Cliente", "%" + cliente + "%");
+                }
+
+                if (desde.HasValue)
+                {
+                    consulta += " AND V.Fecha >= @Desde ";
+                    datos.setearParametro("@Desde", desde.Value);
+                }
+
+                if (hasta.HasValue)
+                {
+                    consulta += " AND V.Fecha < @Hasta ";
+                    datos.setearParametro("@Hasta", hasta.Value.AddDays(1));
+                }
+
+                consulta += " ORDER BY V.Id DESC";
+
+                datos.setearConsulta(consulta);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Venta aux = new Venta();
+
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Fecha = (DateTime)datos.Lector["Fecha"];
+                    aux.Total = (decimal)datos.Lector["Total"];
+
+                    if (!(datos.Lector["NumeroFactura"] is DBNull))
+                    {
+                        aux.FacturaAsociada = new Factura();
+                        aux.FacturaAsociada.NumeroFactura = (string)datos.Lector["NumeroFactura"];
+                    }
+
+                    aux.ClienteAsociado = new Cliente();
+                    aux.ClienteAsociado.Nombre = (string)datos.Lector["Cliente"];
+
+                    aux.RegistradoPor = new Usuario();
+                    if (!(datos.Lector["Vendedor"] is DBNull))
+                    {
+                        aux.RegistradoPor.NombreUsuario = (string)datos.Lector["Vendedor"];
+                    }
+                    else
+                    {
+                        aux.RegistradoPor.NombreUsuario = "Sistema / Sin Asignar";
+                    }
+
+                    lista.Add(aux);
+                }
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public List<DetalleVenta> ListarDetalles(int idVenta)
+        {
+            List<DetalleVenta> lista = new List<DetalleVenta>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                string consulta = @"SELECT DV.Id, P.Nombre as Producto, DV.Cantidad, DV.PrecioUnitario 
+                                    FROM DetalleVentas DV 
+                                    JOIN Productos P ON DV.IdProducto = P.Id 
+                                    WHERE DV.IdVenta = @IdVenta";
+
+                datos.setearConsulta(consulta);
+
+                datos.setearParametro("@IdVenta", idVenta);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    DetalleVenta aux = new DetalleVenta();
+
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Cantidad = (int)datos.Lector["Cantidad"];
+                    aux.PrecioUnitario = (decimal)datos.Lector["PrecioUnitario"];
+                    aux.NombreProducto = (string)datos.Lector["Producto"];
+
+                    aux.Subtotal = aux.Cantidad * aux.PrecioUnitario;
+
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public Venta ObtenerVentaPorId(int idVenta)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT V.Id, V.Fecha, V.NumeroFactura, V.Total, C.Nombre as Cliente, U.NombreUsuario as Vendedor FROM Ventas V JOIN Clientes C ON V.IdCliente = C.Id LEFT JOIN Usuarios U ON V.IdUsuario = U.Id WHERE V.Id = @IdVenta");
+                datos.setearParametro("@IdVenta", idVenta);
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    Venta aux = new Venta();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Fecha = (DateTime)datos.Lector["Fecha"];
+                    aux.Total = (decimal)datos.Lector["Total"];
+
+                    if (!(datos.Lector["NumeroFactura"] is DBNull))
+                    {
+                        aux.FacturaAsociada = new Factura();
+                        aux.FacturaAsociada.NumeroFactura = (string)datos.Lector["NumeroFactura"];
+                    }
+
+                    aux.ClienteAsociado = new Cliente();
+                    aux.ClienteAsociado.Nombre = (string)datos.Lector["Cliente"];
+
+                    aux.RegistradoPor = new Usuario();
+                    if (!(datos.Lector["Vendedor"] is DBNull))
+                    {
+                        aux.RegistradoPor.NombreUsuario = (string)datos.Lector["Vendedor"];
+                    }
+                    else
+                    {
+                        aux.RegistradoPor.NombreUsuario = "Sistema / Sin Asignar";
+                    }
+                    return aux;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
     }
 }
--- Negocio/VentaNegocio.cs
+++ Negocio/VentaNegocio.cs
@@ -1,10 +1,11 @@
-﻿using Dominio;
+using Dominio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TPComercio.Datos;
+using TPComercio.Dominio;
 
 namespace Negocio
 {
@@ -15,6 +16,28 @@ namespace Negocio
             VentaDatos datosVenta = new VentaDatos();
 
             return datosVenta.GuardarVenta(idCliente, total, carrito);
+        }
+
+        public List<Venta> ListarHistorial(string cliente = "", DateTime? desde = null, DateTime? hasta = null)
+        {
+            List<Venta> lista = new List<Venta>();
+            VentaDatos datos = new VentaDatos();
+
+            return lista = datos.ListarHistorial(cliente, desde, hasta);
+        }
+
+        public List<DetalleVenta> ListarDetalles(int idVenta)
+        {
+            VentaDatos datos = new VentaDatos();
+            List<DetalleVenta> lista = new List<DetalleVenta>();
+
+            return lista = datos.ListarDetalles(idVenta);
+
+        }
+        public Venta ObtenerVentaPorId(int idVenta)
+        {
+            VentaDatos datos = new VentaDatos();
+            return datos.ObtenerVentaPorId(idVenta);
         }
     }
 }
