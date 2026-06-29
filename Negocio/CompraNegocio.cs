b0548e77cd671714197bea69c5e61a770f55bd55
LucasCoronado <coronadolucasm@gmail.com>
2026-06-29 19:16:55 -0300

    Metodos ListarHistorial, ListarDetalle y ObtenerCompraPorId en CompraNegocio

--- Negocio/CompraNegocio.cs
+++ Negocio/CompraNegocio.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
-using TPComercio.Dominio;
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
 using TPComercio.Datos;
+using TPComercio.Dominio;
 
 namespace Negocio
 {
@@ -10,6 +12,28 @@ namespace Negocio
         {
             CompraDatos datosCompra = new CompraDatos();
             return datosCompra.GuardarCompra(idProveedor, total, listaDetalles);
+        }
+
+        public List<Compra> ListarHistorial(string proveedor = "", DateTime? desde = null, DateTime? hasta = null)
+        {
+            List<Compra> lista = new List<Compra>();
+            CompraDatos datos = new CompraDatos();
+
+            return lista = datos.ListarHistorial(proveedor, desde, hasta);
+        }
+
+        public List<DetalleCompra> ListarDetalles(int idCompra)
+        {
+            CompraDatos datos = new CompraDatos();
+            List<DetalleCompra> lista = new List<DetalleCompra>();
+
+            return lista = datos.ListarDetalles(idCompra);
+        }
+
+        public Compra ObtenerCompraPorId(int idCompra)
+        {
+            CompraDatos datos = new CompraDatos();
+            return datos.ObtenerCompraPorId(idCompra);
         }
     }
 }
